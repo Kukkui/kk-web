1672aecd4253378c3d15c5e46162ba1362c053e8
Punyawee Pos <punyawee@mintel.tech>
2023-02-10 14:27:35 +0700

    Test GA round2

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      HI KK
+      HI KK2
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
