f570800542fa6c44e0a70ca7470f83b6cce46e4d
Rattapong-ICE <rattapong.rmutt.ice@gmail.com>
2023-03-31 15:58:42 +0700

    add sdk OpenTicket

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,43 @@
+import React, { useState } from 'react';
+
 const Home = () => {
-  return <h1>Home</h1>;
+  const [key, setKey] = useState('');
+  const [customers, setCustomers] = useState('');
+  const [tracking, setTracking] = useState('');
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    window.cxOpenTicket(key, JSON.parse(customers), JSON.parse(tracking));
+  };
+  return (
+    <div>
+      <h1>Home1</h1>
+      <br />
+      <b>cxOpenTicket</b>
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor="cx_identify_key">Key:</label>
+        <input id="cx_identify_key" value={key} onChange={(e) => setKey(e.target.value)} />
+        <br />
+        <label htmlFor="cx_identify_customers">Customers:</label>
+        <textarea
+          style={{width: '1000px', height: '150px'}}
+          id="cx_identify_customers"
+          value={customers}
+          onChange={(e) => setCustomers(e.target.value)}
+        />
+        <br />
+        <label htmlFor="cx_identify_tracking">Ticket:</label>
+        <textarea
+          style={{width: '1000px', height: '150px'}}
+          id="cx_identify_tracking"
+          value={tracking}
+          onChange={(e) => setTracking(e.target.value)}
+        />
+        <br />
+        <button type="submit">Submit cx openTicket</button>
+      </form>
+    </div>
+  );
 };
 
 export default Home;
